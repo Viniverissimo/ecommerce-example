3c2d942c2a472a7e89f5141644bad94cc32048ef
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2022-01-14 17:47:38 -0300

    Responsividade

--- main.js
+++ main.js
@@ -6,10 +6,68 @@ setTimeout(function(){
         infinite: true,
         arrow: true
     });
+    $(".aha-first-regua").slick({
+        arrow: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+        ]
+    });
     $(".aha-three-shelf").slick({
         arrow: true,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+        ]
     });
+    $(".aha-depositions-cards").slick({
+        arrow: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+        ]
+    });
+
+    // MENU MOBILE
+
+    const btnMobile = $(".aha-btn-mobile");
+
+    function toggleMenu() {
+        const nav = $(".aha-header-links")
+        nav.toggleClass("active")
+    }
+
+    $("button.aha-mobile-close").on("click", function(){
+        $(".aha-header-links.active").removeClass("active")
+    })
+    
+    btnMobile.on("click", toggleMenu)
+
 },500)
