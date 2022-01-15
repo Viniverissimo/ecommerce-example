c4ad7d8f5749af357122d8db3beed90ea1e37d1f
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2022-01-15 17:12:34 -0300

    alterações shelf

--- main.js
+++ main.js
@@ -75,51 +75,68 @@ setTimeout(function(){
     var productTitle = ''
     var productValue = ''
     var productImage = ''
+    var productId = ''
+    var productArray = []
 
     $(".aha-product-buyButton[data-id='1']").on("click", function(){
       console.log("clicou")
       productTitle = $("span.aha-name[data-title='Anel banhado a ouro']").attr("data-title")
       productValue = $("span.aha-price-offer[data-value='188']").attr("data-value")
       productImage = $(".aha-product-image img[src='./midia/product-image-1.png']").attr("src")
+      productId = $(".aha-product-buyButton[data-id='1']").attr("data-id")
+      productArray.push({id:productId, value:Number(productValue)})
 
       templateRender()
+      getValue()
     })
     $(".aha-product-buyButton[data-id='2']").on("click", function(){
       console.log("clicou")
       productTitle = $("span.aha-name[data-title='Brinco banhado a ouro']").attr("data-title")
       productValue = $("span.aha-price-offer[data-value='160']").attr("data-value")
       productImage = $(".aha-product-image img[src='./midia/product-image-2.png']").attr("src")
+      productId = $(".aha-product-buyButton[data-id='2']").attr("data-id")
+      productArray.push({id:productId, value:Number(productValue)})
 
       templateRender()
+      getValue()
     })
     $(".aha-product-buyButton[data-id='3']").on("click", function(){
       console.log("clicou")
       productTitle = $("span.aha-name[data-title='Brinco Swarovski']").attr("data-title")
       productValue = $("span.aha-price-offer[data-value='100']").attr("data-value")
       productImage = $(".aha-product-image img[src='./midia/product-image-3.png']").attr("src")
+      productId = $(".aha-product-buyButton[data-id='3']").attr("data-id")
+      productArray.push({id:productId, value:productValue})
 
       templateRender()
+      getValue()
     })
     $(".aha-product-buyButton[data-id='4']").on("click", function(){
       console.log("clicou")
       productTitle = $("span.aha-name[data-title='Corrente com Pingente']").attr("data-title")
       productValue = $("span.aha-price-offer[data-value='150']").attr("data-value")
       productImage = $(".aha-product-image img[src='./midia/product-image-4.png']").attr("src")
+      productId = $(".aha-product-buyButton[data-id='4']").attr("data-id")
+      productArray.push({id:productId, value:productValue})
 
       templateRender()
+      getValue()
     })
     $(".aha-product-buyButton[data-id='5']").on("click", function(){
       console.log("clicou")
       productTitle = $("span.aha-name[data-title='Brinco Diamantado']").attr("data-title")
       productValue = $("span.aha-price-offer[data-value='200']").attr("data-value")
-      productImage = $(".aha-product-image img[src='./midia/product-image-5.png']").attr("src")
+      productImage = $(".aha-product-image img[src='./midia/product-image-3.png']").attr("src")
+      productId = $(".aha-product-buyButton[data-id='5']").attr("data-id")
+      productArray.push({id:productId, value:productValue})
 
       templateRender()
+      getValue()
     })
 
     function templateRender(){
       var template = `
-                      <div class="aha-minicart-product">
+                      <div class="aha-minicart-product" data-id="${productId}">
                         <div class="aha-minicart-imageProduct">
                           <img src="${productImage}">
                         </div>
@@ -128,13 +145,59 @@ setTimeout(function(){
                             <span>${productTitle}</span>
                           </div>
                           <div class="aha-minicart-valueProduct">
-                            <span>R$ ${productValue}</span>
+                            <span class="aha-value" data-value="${productValue}">R$ ${productValue}</span>
                           </div>
+                        </div>
+                        <div class="aha-minicart-close">
+                          <button class="aha-close" data-id="${productId}">X</button>
                         </div>
                       </div>
                       `
       $(".aha-products").prepend(template)
     }
+    var total = ''
+    function getValue(){
+      console.log(productArray)
+
+      total = productArray.reduce((prevVal, elem) => prevVal + elem.value, 0)
+
+      $("p.aha-subtotal span").html(total)
+      valueBar()
+
+      console.log(total)
+    }
+
+    // PROGRESS BAR
+    function valueBar(){
+        var minimoFrete = 400
+        console.log(total, "Total")
+        var calcPercentage = ((total / minimoFrete) * 100).toFixed(2);
+        var missingValue = (minimoFrete - total);
+        console.log(missingValue, "Valor que falta")
+    
+        // BARRA DE PROGRESSO
+        if(minimoFrete - total > 0){
+            document.getElementById("retorno--frete_gratis").innerHTML = minimoFrete - total;
+            $(".barra-preta").css({"width": +calcPercentage+"%"});
+        } else {
+            $(".barra-preta").css({"width":"100%"});
+        }
+        
+        // SACOLA CHEIA
+        if (total > minimoFrete) {
+            $(".aha-text-frete").addClass("aha-fretegratis");
+        } else {
+            $(".aha-text-frete").removeClass("aha-fretegratis");
+        }
+    
+        // SACOLA VAZIA
+        if (total == 0){
+            $(".aha-text-frete").addClass("aha-text-initial"); 
+        } else {
+            $(".aha-text-frete").removeClass("aha-text-initial");
+        }
+    }
+
 
   // MINICART OPEN
 
