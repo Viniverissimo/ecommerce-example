b5daaaecc30799c44e8b72ed09a196424e2a6e5e
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2022-01-15 03:55:49 -0300

    criação do minicart

--- main.js
+++ main.js
@@ -70,4 +70,76 @@ setTimeout(function(){
     
     btnMobile.on("click", toggleMenu)
 
+    // PRODUTOS
+
+    var productTitle = ''
+    var productValue = ''
+    var productImage = ''
+
+    $(".aha-product-buyButton[data-id='1']").on("click", function(){
+      console.log("clicou")
+      productTitle = $("span.aha-name[data-title='Anel banhado a ouro']").attr("data-title")
+      productValue = $("span.aha-price-offer[data-value='188']").attr("data-value")
+      productImage = $(".aha-product-image img[src='./midia/product-image-1.png']").attr("src")
+
+      templateRender()
+    })
+    $(".aha-product-buyButton[data-id='2']").on("click", function(){
+      console.log("clicou")
+      productTitle = $("span.aha-name[data-title='Brinco banhado a ouro']").attr("data-title")
+      productValue = $("span.aha-price-offer[data-value='160']").attr("data-value")
+      productImage = $(".aha-product-image img[src='./midia/product-image-2.png']").attr("src")
+
+      templateRender()
+    })
+    $(".aha-product-buyButton[data-id='3']").on("click", function(){
+      console.log("clicou")
+      productTitle = $("span.aha-name[data-title='Brinco Swarovski']").attr("data-title")
+      productValue = $("span.aha-price-offer[data-value='100']").attr("data-value")
+      productImage = $(".aha-product-image img[src='./midia/product-image-3.png']").attr("src")
+
+      templateRender()
+    })
+    $(".aha-product-buyButton[data-id='4']").on("click", function(){
+      console.log("clicou")
+      productTitle = $("span.aha-name[data-title='Corrente com Pingente']").attr("data-title")
+      productValue = $("span.aha-price-offer[data-value='150']").attr("data-value")
+      productImage = $(".aha-product-image img[src='./midia/product-image-4.png']").attr("src")
+
+      templateRender()
+    })
+    $(".aha-product-buyButton[data-id='5']").on("click", function(){
+      console.log("clicou")
+      productTitle = $("span.aha-name[data-title='Brinco Diamantado']").attr("data-title")
+      productValue = $("span.aha-price-offer[data-value='200']").attr("data-value")
+      productImage = $(".aha-product-image img[src='./midia/product-image-5.png']").attr("src")
+
+      templateRender()
+    })
+
+    function templateRender(){
+      var template = `
+                      <div class="aha-minicart-product">
+                        <div class="aha-minicart-imageProduct">
+                          <img src="${productImage}">
+                        </div>
+                        <div class="aha-minicart-infoProduct">
+                          <div class="aha-minicart-nameProduct">
+                            <span>${productTitle}</span>
+                          </div>
+                          <div class="aha-minicart-valueProduct">
+                            <span>R$ ${productValue}</span>
+                          </div>
+                        </div>
+                      </div>
+                      `
+      $(".aha-products").prepend(template)
+    }
+
+  // MINICART OPEN
+
+  $(".aha-usersInfo-miniCart").on("click", function(){
+    $(".aha-minicart").toggleClass("active")
+  })
+
 },500)
